2d16b0ab4b8d894cedabd3e53c9acc2d8ccdb0a2
mariana.vns <mariana.vns@gmail.com>
2020-09-02 13:25:58 -0300

    Revert "Adicionado o assunto antes do almoço"
    This reverts commit c99e3033c0afae90b8561e5d8a37d7fb1779e747.

--- codigos-do-readme.js
+++ codigos-do-readme.js
@@ -10,31 +10,9 @@
 // const cpf = [072, 200, 684, 5, 0]
 // console.log(cpf[90-88])
 
-// const novaArray = []
-// novaArray[0] = "banana"
-// novaArray[1] = 2
-// novaArray[2] = true
-// console.log(novaArray)
+const novaArray = []
+novaArray[0] = "banana"
+novaArray[1] = 2
+novaArray[2] = true
 
-// const paraFazer = ["Estudar", "Assistir vídeo", "Pesquisar Cursos"]
-// function tempo(arr){
-// for (i = 0; i < arr.length; i++){
-//     console.log(`índice: ${i} - tarefa:  ${arr[i]}`)
-//     }
-// }
-// tempo(paraFazer)
-
-// const femaleRappers = ['Elliot', 'Hill', 'Minaj']
-// console.log(femaleRappers) // terminal: ['Elliot', 'Hill', 'Minaj']
-// femaleRappers.push('Thee Stalion')
-// console.log(femaleRappers) // terminal: ['Elliot', 'Hill', 'Minaj', 'Thee Stalion']
-
-// const trueSingers = ['Knowles', 'Houston', 'Keys', 'Spears']
-// console.log(trueSingers) // terminal: ['Knowles', 'Houston', 'Keys', 'Spears']
-// console.log(`Retiramos a ${trueSingers.pop()} da lista de cantoras`)
-// console.log(trueSingers) // terminal: ['Knowles', 'Houston', 'Keys']
-
-// const coresPreferidas = ['lilas', 'azul', 'rosa', 'branco']
-// console.log(coresPreferidas.indexOf('azul')) // terminal: 1
-// console.log(coresPreferidas.indexOf('branco')) // terminal: 3
-// console.log(coresPreferidas.indexOf('verde')) // terminal: -1
+console.log(novaArray)

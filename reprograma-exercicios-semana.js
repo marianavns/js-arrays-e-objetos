3cdc64d0dd7f360d82bc60e99b07c9da4267694d
mariana.vns <mariana.vns@gmail.com>
2020-09-04 20:02:55 -0300

    Desafio 1 respondido

--- reprograma-exercicios-semana.js
+++ reprograma-exercicios-semana.js
@@ -1,6 +1,8 @@
 // DESAFIO - validador de cartão de crédito
 
-// Todos os números de cartão de crédito têm uma sequência que pode ser validada através de um algoritmo chamado Algoritmo de Luhn. Para validar qualquer número de cartão de crédito (o padrão de 16 dígitos), siga os seguintes passos:
+// Todos os números de cartão de crédito têm uma sequência que pode ser validada através de um algoritmo 
+// chamado Algoritmo de Luhn. Para validar qualquer número de cartão de crédito (o padrão de 16 dígitos), 
+// siga os seguintes passos:
 
 // 1. Retire o último dígito do número. Ele é o verificador;
 // 2. Escreva os números na ordem inversa;
@@ -9,14 +11,47 @@
 // 5. O dígito verificador (aquele do passo 1) é o número que você precisa somar a todos os outros números somados para fazer a validação;
 // 6. O número de cartão de crédito será válido caso o total (do passo 5) seja múltiplo de 10 (ou seja, numero % 10 === 0)
 
+
 // Números de cartões válidos para teste:
 
 // 5555666677778884
 // 5485755481460022
-// 5141331902596939
+// 5141331902596939 
 // 5381579886310193
-// 5261400319746371
+// 5261400319746371 
 
+function validaNumero(numero) {
+
+    let arrayCartao = numero.split('') 
+    let arrayCartaoInversa = arrayCartao.reverse('')
+    let verificador = arrayCartao.pop()
+    let mulDois = []
+                for (i = 0; i < arrayCartaoInversa.length; i++){
+                        if ((i % 2 == 0)){
+                        mulDois.push(arrayCartaoInversa[i]*2)
+                        }
+                }           
+    let subNove = []
+            for (i = 0; i < mulDois.length; i++){
+                if (mulDois[i] > 9){
+                subNove.push(mulDois[i] - 9)
+                }
+            }
+    let total = subNove.reduce((acumulador, numero, indice, original) => {
+            return acumulador += numero;
+            }, 0)    
+    let totalComVerificador = parseInt(total) + parseInt(verificador)
+    
+    if (totalComVerificador % 10 == 0){
+                console.log("Cartão Válido")
+            } else {
+                console.log("Cartão Inválido")
+            }
+    
+    } 
+    
+    validaNumero("5141331902596939")
+    
 ///////////////////////
 
 // DESAFIO - saudar clientes
@@ -40,4 +75,3 @@
 // if (nomeCliente === 'Maria') {
 //   // etc
 // }
-
